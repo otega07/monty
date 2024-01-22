22fdce726d7321006c859a89f8f961c0fdf7e53a
otega07 <okiriotega@gmail.com>
2024-01-22 02:34:59 +0100

    task 7

--- div.c
+++ div.c
@@ -26,7 +26,7 @@ void f_div(stack_t **head, unsigned int counter)
 	h = *head;
 	if (h->n == 0)
 	{
-		fprintf(stderr, "L%d: div by zero\n", counter);
+		fprintf(stderr, "L%d: division by zero\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
