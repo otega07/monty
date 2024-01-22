92fc27b63559f1286e6f8aae9d5f48d670b1bece
otega07 <okiriotega@gmail.com>
2024-01-22 02:55:23 +0100

    task 0

--- push.c
+++ push.c
@@ -7,30 +7,34 @@
  */
 void push(stack_t **head, unsigned int counter)
 {
-	int n;
-	int j = 0;
-	int flag = 0;
+	int n, j = 0, flag = 0;
 
-	if (bus.arg)
+	if (bus.arg != NULL)
 	{
 		if  (bus.arg[0] == '-')
 			j++;
 		for (; bus.arg[j] != '\0'; j++)
 		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
+			if (bus.arg[j] > '9' || bus.arg[j] < '0')
+				flag = 1;
+		}
 		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push inteer\n", counter);
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
-			exit(EXIT_FAILURE); }}
+			exit(EXIT_FAILURE);
+		}
+	}
 	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE); }
+		exit(EXIT_FAILURE);
+	}
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
 		addnode(head, n);
